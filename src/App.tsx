7423a1b534397daaf99dbd3d6ab0f6729aaa0031
chocomong <chocomonq@gmail.com>
2023-01-06 01:23:56 +0900

    AnimatePresence : Slider part 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { AnimatePresence, motion } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
 	display: flex;
+	flex-direction: column;
 	align-items: center;
 	justify-content: center;
 	width: 100vw;
@@ -12,41 +13,66 @@ const Wrapper = styled(motion.div)`
 `;
 
 const Box = styled(motion.div)`
+	display: flex;
+	align-items: center;
+	justify-content: center;
 	position: absolute;
-	top: 200px;
+	top: 100px;
 	width: 400px;
 	height: 200px;
 	border-radius: 30px;
 	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 	background-color: rgba(255, 255, 255, 1);
+	font-size: 28px;
 `;
 
-const boxVariants = {
-	initial: {
-		opacity: 0,
+const box = {
+	invisible: {
 		scale: 0,
+		opacity: 0,
+		x: 500,
 	},
-	visible: { opacity: 1, scale: 1, rotateZ: 360 },
-	leaving: { opacity: 0, scale: 0, y: 50 },
+	visible: {
+		scale: 1,
+		opacity: 1,
+		x: 0,
+		transition: {
+			duration: 1,
+		},
+	},
+	exit: {
+		scale: 0,
+		opacity: 0,
+		x: -500,
+		transition: {
+			duration: 1,
+		},
+	},
 };
 
 function App() {
-	const [showing, setShowing] = useState(false);
-	const toggleShowing = () => setShowing((prev) => !prev);
-
+	const [visible, setVisible] = useState(1);
+	const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+	const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
 	return (
 		<Wrapper>
 			<AnimatePresence>
-				{showing ? (
-					<Box
-						variants={boxVariants}
-						initial="initial"
-						animate="visible"
-						exit="leaving"
-					/>
-				) : null}
+				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+					i === visible ? (
+						<Box
+							variants={box}
+							initial="invisible"
+							animate="visible"
+							exit="exit"
+							key={i}
+						>
+							{i}
+						</Box>
+					) : null
+				)}
 			</AnimatePresence>
-			<button onClick={toggleShowing}>Click Me</button>
+			<button onClick={nextPlease}>Next</button>
+			<button onClick={prevPlease}>Prev</button>
 		</Wrapper>
 	);
 }
